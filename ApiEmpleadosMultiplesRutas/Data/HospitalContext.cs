606cec7835cf60231fc6d9a2d903422330a4d9f3
serraguti <pacoserranox@gmail.com>
2023-04-14 09:53:29 +0200

    Add project files.

---
+++ ApiEmpleadosMultiplesRutas/Data/HospitalContext.cs
@@ -0,0 +1,12 @@
+﻿using ApiEmpleadosMultiplesRutas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiEmpleadosMultiplesRutas.Data
+{
+    public class HospitalContext:DbContext
+    {
+        public HospitalContext(DbContextOptions<HospitalContext> options)
+            : base(options) { }
+        public DbSet<Empleado> Empleados { get; set; }
+    }
+}
---
+++ ApiEmpleadosMultiplesRutas/Repositories/RepositoryHospital.cs
@@ -0,0 +1,50 @@
+﻿using ApiEmpleadosMultiplesRutas.Data;
+using ApiEmpleadosMultiplesRutas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiEmpleadosMultiplesRutas.Repositories
+{
+    public class RepositoryHospital
+    {
+        private HospitalContext context;
+
+        public RepositoryHospital(HospitalContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Empleado>> GetEmpleadosAsync()
+        {
+            return await this.context.Empleados.ToListAsync();
+        }
+
+        public async Task<Empleado> FindEmpleadoAsync(int idempleado)
+        {
+            return await
+                this.context.Empleados.FirstOrDefaultAsync
+                (x => x.IdEmpleado == idempleado);
+        }
+
+        public async Task<List<string>> GetOficiosAsync()
+        {
+            var consulta = (from datos in this.context.Empleados
+                            select datos.Oficio).Distinct();
+            return  await consulta.ToListAsync();
+        }
+
+        public async Task<List<Empleado>> GetEmpleadosOficioAsync(string oficio)
+        {
+            return await
+                this.context.Empleados.Where(z => z.Oficio == oficio).ToListAsync();
+        }
+
+        public async Task<List<Empleado>> 
+            GetEmpleadosSalarioAsync(int salario, int iddepartamento)
+        {
+            return await
+                this.context.Empleados.Where(x => x.Salario >= salario
+                && x.IdDepartamento == iddepartamento)
+                .ToListAsync();
+        }
+    }
+}
